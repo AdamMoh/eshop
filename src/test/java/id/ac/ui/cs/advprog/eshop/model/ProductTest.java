4e641837a521ef5342a5ce06f7d6018ee75085d7
AdamMoh <xlkartu12345@gmail.com>
2024-02-06 17:43:06 +0700

    add functional test

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -2,7 +2,7 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.*;
 class ProductTest {
 
     Product product;
