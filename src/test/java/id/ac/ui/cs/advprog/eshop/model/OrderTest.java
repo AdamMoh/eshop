0637ba900f067513eee82f727e0978a74d627973
AdamMoh <xlkartu12345@gmail.com>
2024-03-05 14:05:03 +0700

    [RED] Add Order model skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -70,13 +70,10 @@ class OrderTest {
     }
 
     @Test
-
     void testSetStatusToInvalidStatus() {
         Order order = new Order ("13652556-012a-4c07-b546-54eb1396d79b",
                 this.products, 1708560000L,"Safira Sudrajat");
         assertThrows (IllegalArgumentException.class, () -> order.setStatus("MEOW"));
     }
-
-
 
 }
